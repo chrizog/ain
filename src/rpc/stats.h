8b5b21ae08a4fb84fc86c8b27c30084eb8598d25
jouzo <jdesclercs@gmail.com>
2021-12-09 22:26:07 +0100

    Introduce new CRPCStats class

---
+++ src/rpc/stats.h
@@ -0,0 +1,28 @@
+#ifndef DEFI_RPC_STATS_H
+#define DEFI_RPC_STATS_H
+
+#include <map>
+#include <stdint.h>
+#include <univalue.h>
+#include <util/time.h>
+
+/**
+ * DeFi Blockchain RPC Stats class.
+ */
+class CRPCStats
+{
+private:
+    std::map<std::string, UniValue> map;
+public:
+    void add(const std::string& name, const int64_t latency, const size_t payload);
+
+    const UniValue get(const std::string& name) { return map[name]; };
+
+    std::map<std::string, UniValue> getList() { return map; };
+
+    // void save();
+};
+
+extern CRPCStats statsRPC;
+
+#endif // DEFI_RPC_STATS_H
